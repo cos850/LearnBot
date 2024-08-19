f4246d918d14a3ae285711514d5ab20393d93531
cos850 <cos850@naver.com>
2024-08-20 07:37:30 +0900

    fix: 자동 로그인 오류 수정 (iframe 내부 탐색)

--- learnBot-autoLogin.py
+++ learnBot-autoLogin.py
@@ -16,26 +16,35 @@ try :
     # 웹 사이트 열기
     driver.get(Config.login_url)
     time.sleep(5)
+    
+    # iframe 전환
+    loginIframe = driver.find_element(By.XPATH, '//iframe[@id="certframe"]')
+    driver.switch_to.frame(loginIframe)
 
     # 법인 공인인증서 로그인 1단계 : ID 입력
-    username_input = driver.find_element(By.XPATH, '//*[@id="sub_common_id"]')
-    login_id_button = driver.find_element(By.XPATH, '//*[@id="login_common_btn"]')
+    username_input = driver.find_element(By.ID, 'sub_common_id')
+    login_id_button = driver.find_element(By.ID, 'login_common_btn')
 
     username_input.send_keys(Config.my_id)
-
     login_id_button.click()
-    time.sleep(2)
-
+    time.sleep(1)
+    
     # 범용 공인인증서 로그인 2단계 : 공인인증서 비번 입력
     password_input = driver.find_element(By.ID, 'certPwd')
-    password_input.send_keys(Config.my_pw)
-
     login_pw_button = driver.find_element(By.XPATH, '//div[@class="pki-bottom"]/button[@class="btn-ok"]')
+    
+    password_input.send_keys(Config.my_pw)
     login_pw_button.click()
 
     # 로그인 후 페이지가 로드될 시간을 기다림
-    time.sleep(5)  # 5초 대기, 필요에 따라 조정
-finally:
+    time.sleep(5)
+    driver.switch_to.default_content()
+except Exception as e:
+    print('로그인 오류 발생!!!!!!!!')
+    print(e)
     print(driver.page_source)
+    driver.switch_to.default_content()
+finally:
+    print("프로그램 종료")
     time.sleep(20)
     driver.quit()
