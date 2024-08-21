8e7229a55ebeb51a044a3ec669998bb1bc4455d3
cos850 <cos850@naver.com>
2024-08-21 21:50:22 +0900

    refactor: 미사용 속성 제거 및 함수 추출, Lecture 클래스 정의

--- features/auto_learn.py
+++ features/auto_learn.py
@@ -1,10 +1,10 @@
+from typing import List
 from selenium.webdriver.remote.webdriver import WebDriver, WebElement
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import *
 from utils.driver_manager import DriverManager
-from config.settings import COURSE_LIST, Course
+from utils.learning import Course, Lecture
+from config.settings import COURSE_LIST
 from time import sleep
 import copy
 import traceback
@@ -17,34 +17,36 @@ def learn():
     print('강의 수강 시작')
     try:
         global current_course
-        options = driver.find_element(By.ID, 'Select_ApplCnt').find_elements(By.TAG_NAME, 'option')
-        goCourseBtn = driver.find_element(By.ID, 'btnSelectApplCnt')
+        course_options = driver.find_element(By.ID, 'Select_ApplCnt').find_elements(By.TAG_NAME, 'option')
+        go_course_btn = driver.find_element(By.ID, 'btnSelectApplCnt')
         
-        for option in options :
+        for option in course_options :
             value = option.get_property('value')
             
             current_course = _find_course(value)
             if current_course == None : 
                 print(f'[{option.text}] 과목에 대한 Course 정보를 찾을 수 없음. 해당 강의 스킵')
+            elif current_course.is_completed :
+                print(f'[{current_course.name}] 과목은 이미 수강 완료되었습니다.')
             else :
                 print(f'[{option.text}] 과목의 학습 페이지로 이동합니다.')
                 option.click()
-                goCourseBtn.click()
+                go_course_btn.click()
                 sleep(5)
                 
-                # 미완료 강의들 수강하기
-                lectures = _find_uncompleted_lectures()
+                # 수강할 강의 목록 탐색
+                lectures: List[Lecture] = _find_uncompleted_lectures()
                 
-                # 확인용 임시 출력 ------
-                print('수강할 목록: ') 
-                for a_tag in lectures:
-                    print(a_tag.find_element(By.TAG_NAME, 'b').text, ': ', a_tag.get_property('href'))
-                print('===============')
-                # -----------------------
+                print(f'\n=== [{current_course.name}] 미수료 강의 목록 ===') 
+                print('\n'.join(lecture.name for lecture in lectures))
+                print('===========================================\n')
                 
                 for lecture in lectures :
-                    print(f'강의 듣기 시작 - {current_course.name}-{lecture.find_element(By.TAG_NAME, 'b').text}')
+                    print(f'[{current_course.name}] {lecture.name} 강의 수강 시작')
                     _play(lecture)
+                    
+                current_course.is_completed = True
+                print(f'{current_course.name} 과목은 완료처리 되었습니다.')
         
         print('모든 강의를 수강했습니다.')  
     except Exception as e:
@@ -59,16 +61,15 @@ def _find_course(value):
                 return course
 
 
-# 수강할 강의 목록 찾기
-def _find_uncompleted_lectures():
+def _find_uncompleted_lectures() -> List[Lecture]: 
     table = driver.find_element(By.ID, 'dataList')
     
     # 수강이 가능할 경우 a 태그가 있고, fontcolor가 '#588BA5'
     open_lectures = table.find_elements(By.XPATH, '//tbody//tr//td[@class="alignL"]//a[font[@color="#588BA5"]]')
     
-    # 진행여부 열에 값이 '완료'인 경우는 제외
-    return [lec for lec in open_lectures if not _isDone(lec)]
+    return [Lecture(name=lec.find_element(By.TAG_NAME, 'b').text, tag=lec) for lec in open_lectures if not _isDone(lec)]
 
+# 진행 여부 열이 '완료' 되어있는지 확인
 def _isDone(lecture):
     parent_tr = lecture.find_element(By.XPATH, '..').find_element(By.XPATH, '..')
     status_td = parent_tr.find_elements(By.TAG_NAME, 'td')[4]
@@ -81,11 +82,11 @@ def _isDone(lecture):
     
     
 # 강의 듣기
-def _play(lecture: WebElement):
+def _play(lecture: Lecture):
     main_window_handle = driver.current_window_handle
     
     try:
-        lecture.click()
+        lecture.tag.click()
         sleep(3)
         
         # 학습 페이지가 새 창으로 띄워짐. 해당 window로 전환
@@ -93,47 +94,41 @@ def _play(lecture: WebElement):
         lecture_window_handle = [handle for handle in all_window_handles if handle != main_window_handle][-1]
         driver.switch_to.window(lecture_window_handle)
         
-        driver.switch_to.frame('mainHaksup')    # 메인 학습 frame 내부로 전환
-        _progress_all_step()                    # 강의 진행
+        _progress_all_step()
         
-        # 상단바 frame으로 전환 (학습 상태 확인 및 종료)
-        driver.switch_to.default_content()
-        driver.switch_to.frame('chkTime')
+        _wait_until_100_percent()
         
-        # 학습 시간 충족 확인 및 대기
-        is_time_fulfilled = _is_100_percent()
-        while not is_time_fulfilled: 
-            print(f'강의 수강 중.... [학습시간{_getPercent()}]')
-            sleep(10*60) # 마지막 페이지에서 10분 기다리기
-            is_time_fulfilled = _is_100_percent()
-        print('학습 시간 충족. 해당 강의 학습을 종료합니다.')
+        _close_lecture()
         
-        # 학습 종료 버튼 클릭
-        driver.find_element(By.XPATH, '//div[@class="myStudy"]/p/a').click()
-        
-        # 학습을 종료하시겠습니까? -> 확인
-        driver.switch_to.alert.accept() 
-        
-        # 종료하기 버튼 클릭
-        driver.execute_script('StopStudy()')
-        
-        print('수강 완료')
+        print(f'{lecture.name} - 수강 완료')
     finally: 
         # 기존 창으로 돌아가기
         driver.switch_to.window(main_window_handle)
         sleep(2) # 페이지 재로딩 대기
 
+
+# 강의 듣기
+# 마지막 페이지라는 alert창이 뜰 때까지 진행
 def _progress_all_step():
-    while True :
-        isDone = _handle_alert()
-        if isDone :
-            return
-        else :
+    driver.switch_to.frame('mainHaksup')
+    try :
+        while not _handle_alert() :
             driver.find_element(By.XPATH, '//*[@id="title_frame"]/a[2]').click()
             sleep(1)    # 너무 빨리 누르지 않도록 대기
+    finally :
+        driver.switch_to.default_content()
 
-def _is_100_percent():
-    return _getPercent() == ': 100%'
+
+# 학습시간 100% 까지 대기
+def _wait_until_100_percent():
+    driver.switch_to.frame('chkTime')
+    try :
+        while not _getPercent() == ': 100%': 
+            print(f'강의 수강 중.... [학습시간{_getPercent()}]')
+            sleep(10*60) # 마지막 페이지에서 10분 기다리기
+        print('학습 시간 충족. 해당 강의 학습을 종료합니다.')
+    finally :
+        driver.switch_to.default_content()
 
 def _getPercent():
     my_study = driver.find_element(By.CLASS_NAME, 'myStudy')
@@ -154,6 +149,23 @@ def _handle_alert():
         return False
     return False
 
+def _close_lecture() :
+    driver.switch_to.frame('chkTime')
+    try :
+        # 학습 종료 버튼 클릭
+        driver.find_element(By.XPATH, '//div[@class="myStudy"]/p/a').click()
+        
+        # 학습을 종료하시겠습니까? -> 확인
+        driver.switch_to.alert.accept() 
+        
+        # 종료하기 버튼 클릭
+        driver.switch_to.default_content()
+        driver.switch_to.frame('mainHaksup')
+        
+        driver.execute_script('StopStudy()')
+    finally :
+        driver.switch_to.default_content()
+
 def _take_quiz():
     print('퀴즈 풀기')
     
@@ -170,7 +182,7 @@ def _selectAnswer(quiz_num):
     quiz_area = driver.find_element(By.ID, f'quiz_{quiz_num}')
     
     answer = quiz_area.find_element(By.ID, 'dap').get_property('value') # 정답 번호 가져오기
-    print(f'quiz {quiz_num} - answer : {answer}')
+    print(f'{current_course.name} quiz {quiz_num} - answer : {answer}')
     
     quiz_area.find_element(By.ID, f'a{answer}').click() # 정답 번호 클릭
     
--- utils/learning.py
+++ utils/learning.py
@@ -1,10 +1,13 @@
-from typing import List
 from selenium.webdriver.remote.webdriver import WebElement
 
 class Course:
     is_completed = False
-    uncompleted_studies: List[WebElement] = []
     
     def __init__(self, value, name):
         self.value = value
         self.name = name
+        
+class Lecture:
+    def _init_(self, name, tag: WebElement):
+        self.name = name
+        self.tag = tag
